bc0a551847a4326cc08b79def735e32d24d49c7f
serj3191 <serhiitynovskyi@gmail.com>
2020-11-10 11:16:46 -0600

    initial commit 3

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("How are you?");
+        System.out.println("Green line shows new stuff");
     }
 }
