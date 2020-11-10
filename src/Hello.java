1bf63c609e7b5104147cc20eff0ae1c998104abd
serj3191 <serhiitynovskyi@gmail.com>
2020-11-10 11:49:13 -0600

    project shared in remote repo.

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,8 @@ public class Hello {
 
     public static void main(String[] args) {
         // new
-
+// let's do more commits
+        // do not need to share it agaIN
+        //ONLY COMMIT AND PUSH
     }
 }
